74dfd801466a70e5a974d031ca9e5eac87943ad3
Juanlu3095 <jcooldevelopment@gmail.com>
2025-09-24 13:20:14 +0200

    Formulario simple para mensajes en la home

--- projects/frontend/src/app/client/home/home.component.ts
+++ projects/frontend/src/app/client/home/home.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  contactForm = new FormGroup({
+    nombre: new FormControl('', Validators.compose([Validators.min(1), Validators.required])),
+    apellidos: new FormControl('', Validators.compose([Validators.min(1), Validators.required])),
+    telefono: new FormControl('', Validators.compose([Validators.min(1), Validators.required])),
+    email: new FormControl('', Validators.compose([Validators.min(1), Validators.required, Validators.email])),
+    contenido: new FormControl('', Validators.compose([Validators.min(1), Validators.required]))
+  })
 }
